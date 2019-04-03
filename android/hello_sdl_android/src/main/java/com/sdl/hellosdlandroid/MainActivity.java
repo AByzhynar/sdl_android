3c8e6585d78b26ebb94e01b7e2520609e472f503
Andriy Byzhynar (GitHub) <AByzhynar@luxoft.com>
2019-04-03 15:11:16 +0300

    Initial commit
    Added radio buttons. Not yet handled
    Added PublishChosen Service button
    Added OnAppService button
    Added view for logging

--- android/hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/MainActivity.java
+++ android/hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/MainActivity.java
@@ -1,24 +1,65 @@
 package com.sdl.hellosdlandroid;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 	private static final String TAG = "MainActivity";
-	
+	boolean mBound = false;
+	//private Intent proxyIntent = null;
+	SdlService mService;
+	TextView tvOut;
+	TextView outputText;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		tvOut = (TextView) findViewById(R.id.tvOut);
+		outputText = (TextView) findViewById(R.id.terminalOutput);
+		outputText.setMovementMethod(new ScrollingMovementMethod());
+		outputText.append("\n"+"MainActivity::onCreate");
+
+	}
+
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		outputText.append("\n" + "MainActivity::onStart");
 		//If we are connected to a module we want to start our SdlService
 		if(BuildConfig.TRANSPORT.equals("MULTI") || BuildConfig.TRANSPORT.equals("MULTI_HB")) {
+			outputText.append("\n"+"MainActivity::SdlReceiver.queryForConnectedService");
 			SdlReceiver.queryForConnectedService(this);
 		}else if(BuildConfig.TRANSPORT.equals("TCP")) {
+			outputText.append("\n"+"MainActivity::startService");
 			Intent proxyIntent = new Intent(this, SdlService.class);
 			startService(proxyIntent);
+			// Bind to SdlService
+			bindService(proxyIntent, mConnection, Context.BIND_AUTO_CREATE);
+			outputText.append("\n"+"MainActivity::Binding Service");
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		outputText.append("\n"+"MainActivity::onStop");
+		super.onStop();
+		// Unbind from the service
+		if (mBound) {
+			unbindService(mConnection);
+			mBound = false;
 		}
 	}
 
@@ -40,4 +81,46 @@ public class MainActivity extends AppCompatActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+
+	/** Called when a button is clicked (the button in the layout file attaches to
+	 * this method with the android:onClick attribute) */
+	public void sendPASRequest(View v) {
+		tvOut.setText("Нажата кнопка Publish Chosen Service");
+		if (mBound) {
+			tvOut.setText("Service bound ");
+			// Call a method from the LocalService.
+			// However, if this call were something that might hang, then this request should
+			// occur in a separate thread to avoid slowing down the activity performance.
+			//mService.publishAppServiceRequest();
+
+		}
+	}
+
+	public void checkButtonWork(View view) {
+		tvOut.setText("Нажата кнопка OnAppService Data");
+	}
+
+	/** Defines callbacks for service binding, passed to bindService() */
+	private ServiceConnection mConnection = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName className,
+									   IBinder service) {
+			outputText.append("\n"+"MainActivity::OnServiceConnected");
+			// We've bound to SdlService, cast the IBinder and get SdlService instance
+//			SdlService.LocalBinder binder = (SdlService.LocalBinder) service;
+//			mService = binder.getService();
+			mBound = true;
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName className) {
+			outputText.append("\n"+"MainActivity::OnServiceDisconnected");
+			mBound = false;
+			mService = null;
+		}
+	};
+
+
 }
