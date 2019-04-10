ac9d81aa5d4b0f38d53069e7e0a0403e9d4e8c1a
Andriy Byzhynar (GitHub) <AByzhynar@luxoft.com>
2019-04-10 12:19:50 +0300

    Disable landscape mode
    Landscape mode is disable due to incorrect app behavior
    Added always screen on for app

--- android/hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/MainActivity.java
+++ android/hello_sdl_android/src/main/java/com/sdl/hellosdlandroid/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.view.WindowManager;
 
 import com.smartdevicelink.proxy.rpc.enums.AppHMIType;
 
@@ -38,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         instance = new WeakReference<MainActivity>(this);
         spinner = (Spinner) findViewById(R.id.spinner1);
         // Spinner click listener
